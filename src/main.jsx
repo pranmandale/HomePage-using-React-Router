7d139c305b0bd08749342f88985c24590a9f1e3f
pranmandale <pranavmandale2025@gmail.com>
2024-07-01 11:19:58 +0530

    new files added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/about/About.jsx'
+import User from './components/user/User.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+    <Route path='' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+    {/* <Route path="company" element={<About />}/> */}
+    {/* </Route> */}
+    <Route path="user" element={<User/>} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
+
